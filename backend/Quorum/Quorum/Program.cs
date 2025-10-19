146800a7125439da9eae3e41a36ff20e7384d35f
Luan <marceluan@gmail.com>
2025-10-19 11:58:30 -0300

    Fixing api ports and add csv files

--- backend/Quorum/Quorum/Program.cs
+++ backend/Quorum/Quorum/Program.cs
@@ -64,7 +64,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Legislative Data API V1");
-        c.RoutePrefix = string.Empty; // Swagger at root
+        c.RoutePrefix = "swagger";
     });
 }
 
